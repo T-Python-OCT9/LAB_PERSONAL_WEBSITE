1b23db288478cc60a0131a71f8934daa89323c7f
Athari_Alharbi <AthariAlharbiA@hotmail.com>
2022-10-31 20:59:43 +0300

    WEB+

---
+++ Athari/MyWebSite/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+def about_me(request:HttpRequest):
+
+    text1 = "Athari Alharbi: "    
+    return render(request, 'MyWebSite/index.html', {"text" : text1})
+    
+
+
+def education(request:HttpRequest):
+
+    text_education = ""
+
+    text_certificates = ""
+
+    return HttpResponse(text_education,text_certificates) 
+
+
+def trainig_programs(request:HttpRequest):
+
+    train1  = "Acquired training programs" 
+   
+
+    return render(request, 'MyWebSite/train.html', {"train" : train1 }) 
+
+
+def skills(request:HttpRequest): 
+
+    
+
+    return HttpResponse()           
