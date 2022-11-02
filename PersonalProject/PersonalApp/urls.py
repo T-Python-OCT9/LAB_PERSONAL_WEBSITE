771ea31b60af78180399cf5b862aa6ed792fa4c5
Roaa Albishri <roaa20101@hotmail.com>
2022-11-02 09:27:50 +0300

    Updated LAB_PERSONAL_WEBSITE

--- PersonalProject/PersonalApp/urls.py
+++ PersonalProject/PersonalApp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'PersonalApp'
 urlpatterns = [
-    path('about/', views.aboutMe, name="about"),
     path('home/', views.home, name="home"),
     path('resume/', views.resume, name="resume"),
+    path('work/', views.work, name="work"),
     path('contact/', views.contact, name="contact"),
 
 
--- PersonalProject/PersonalApp/views.py
+++ PersonalProject/PersonalApp/views.py
@@ -1,22 +1,15 @@
-from multiprocessing import context
+
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
-
-def aboutMe(request:HttpRequest):
-    context = {
-        'name': "Roaa Albishri"
-    }
-    return render(request,'PersonalApp/base.html' , context)
 
 def home(request:HttpRequest):
 
-    return render(request,'PersonalApp/Home.html')
+    return render(request,'PersonalApp/home.html')
 
 def resume(request:HttpRequest):
 
-    return render(request,'PersonalApp/resume.html' )
+    return render(request,'PersonalApp/Resume.html' )
 
 def work(request:HttpRequest):
 
